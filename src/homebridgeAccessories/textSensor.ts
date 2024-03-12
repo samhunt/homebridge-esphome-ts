c7c17c0d5857fb13ab9025aa776bbc5805a0eb0e
Sam Hunt <sam@samhunt.co>
2024-03-12 20:20:25 +1100

    bugfix

--- src/homebridgeAccessories/textSensor.ts
+++ src/homebridgeAccessories/textSensor.ts
@@ -2,12 +2,14 @@ import { Characteristic, Service } from '../index';
 import { PlatformAccessory, Service as HAPService } from 'homebridge';
 
 export const textSensorHelper = (component: any, accessory: PlatformAccessory): boolean => {
-    
     // not sure Diagnostics makes the most sense here, but it's the closest I can find ATM 
-    let sensor = accessory.addService(new  Service.Diagnostics(component.name, ''));
+    let sensor = accessory.services.find((service) => service.UUID === Service.Diagnostics.UUID);
+    if (!sensor) { 
+        sensor = accessory.addService(new  Service.Diagnostics(component.name, ''));
+    }
     
     component.on('state', (state: any) => {
-        sensor.getCharacteristic(Characteristic.SupportedDiagnosticsSnapshot).setValue(state.state);
+        sensor?.getCharacteristic(Characteristic.SupportedDiagnosticsSnapshot)?.setValue(state.state);
     });
     return true;
 };
